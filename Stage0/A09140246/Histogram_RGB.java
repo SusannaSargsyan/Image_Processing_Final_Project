7c6f1c3ca2209c84bcfc1b8659286b9bc5f341a2
susannasargsian <susann_sargsyan@edu.aua.am>
2019-12-17 03:18:06 +0400

    Stage 2, Step 1, Step2p

---
+++ Stage0/A09140246/Histogram_RGB.java
@@ -0,0 +1,55 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+import java.awt.*;
+
+public class Histogram_RGB implements PlugInFilter {
+
+    //R G B
+    double[] red = new double[256];
+    double[] green = new double[256];
+    double[] blue = new double[256];
+
+    /**
+     * Set up
+     * @param args
+     * @param im
+     * @return
+     */
+    public int setup(String args, ImagePlus im) {
+        return DOES_RGB;
+    }
+
+
+    public void run(ImageProcessor ip) {
+        for (int i = 0; i < ip.getWidth(); i++) {
+            for (int j = 0; j < ip.getHeight(); j++) {
+                Color color = new Color(ip.getPixel(i, j));
+                red[color.getRed()] += 1;
+                blue[color.getBlue()] += 1;
+                green[color.getGreen()] += 1;
+            }
+        }
+
+        red[0] = red[0] / ip.getWidth() * ip.getHeight();
+        green[0] = green[0] / ip.getWidth() * ip.getHeight();
+        blue[0] = blue[0] / ip.getWidth() * ip.getHeight();
+
+        for (int i = 1; i < red.length; i++) {
+            red[i] = (red[i] / (ip.getWidth() * ip.getHeight())) + red[i - 1];
+            green[i] = (green[i] / (ip.getWidth() * ip.getHeight())) + green[i - 1];
+            blue[i] = (blue[i] / (ip.getWidth() * ip.getHeight())) + blue[i - 1];
+        }
+
+      
+
+        IJ.log("Greens counts: ");
+        for (int i = 0; i < green.length; i++) {
+            IJ.log(Double.toString(green[i]));
+        }
+
+      
+    }
+}
